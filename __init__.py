57be3f2595a1b03d5d1df9a1f4cfd135216119b1
Tom Farley <tpmf500@york.ac.uk>
2018-01-04 17:06:01 +0000

    Make logger loading more robust

--- __init__.py
+++ __init__.py
@@ -6,9 +6,12 @@ import os
 import sys
 
 print('__init__: {}'.format(os.getcwd()))
-fn_log = '../logging_config.ini'
+fn_log = 'logging_config.ini'
 try:
+    assert os.path.isfile(fn_log)
     fileConfig(fn_log)
-except:
-    print('Failed to load logger settings from {}'.format(fn_log))
+except KeyError as e:
+    print('Failed to load logger settings from {}\nKeyError: {}'.format(fn_log, e))
+except AssertionError as e:
+    print('Failed to load logger settings from {}\nFile does not exist {}'.format(fn_log, e))
 logging.getLogger(__name__).addHandler(logging.NullHandler())
