80d1f17ec8ae37a8163c8529480fb25ceffe2d57
Tom Farley <tpmf500@york.ac.uk>
2018-01-04 16:51:24 +0000

    Make logger loading more robust

--- __init__.py
+++ __init__.py
@@ -2,5 +2,12 @@
 
 import logging
 from logging.config import fileConfig
-fileConfig('../logging_config.ini')
+import os
+import sys
+print(os.getcwd())
+fn_log = '../logging_config.ini'
+try:
+    fileConfig(fn_log)
+except:
+    print('Failed to load logger settings from {}'.format(fn_log))
 logging.getLogger(__name__).addHandler(logging.NullHandler())
